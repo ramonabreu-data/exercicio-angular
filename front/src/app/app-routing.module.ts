e7a09d39367f14e277e9394b24a8f750957d34e3
ramonabreu-data <cdevabreu@gmail.com>
2023-02-02 16:36:28 -0300

    006-RI-RamonAbreu("Criação Rota de Login")

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginRoutes } from './auth/auth-routing.module';
 
 import { LoginComponent } from './pages/login/login.component';
 import { PessoasComponent } from './pages/pessoas/pessoas.component';
 import { SetoresComponent } from './pages/setores/setores.component';
 
 const routes: Routes = [
-  { path:"login", component: LoginComponent, title: "Login" },
-  { path:"pessoas", component: PessoasComponent, title: "Pessoas" },
-  { path:"setores", component: SetoresComponent, title: "Setores" },
+  { path: "",
+  redirectTo: "login",
+   pathMatch: 'full'
+},
+  { path:"login",
+   component: LoginComponent, 
+   title: "Login"
+ },
+  { path:"pessoas", 
+  component: PessoasComponent,
+   title: "Pessoas" 
+},
+  { path:"setores", 
+  component: SetoresComponent, 
+  title: "Setores"
+ },
 
-  { path: "", redirectTo: "/login", pathMatch: 'full'}
+ 
+...LoginRoutes
 ];
 
 @NgModule({
