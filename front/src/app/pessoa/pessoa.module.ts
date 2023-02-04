e495a764b93f7829b0bbe6e3f1ce446b3a00c358
ramonabreu-data <cdevabreu@gmail.com>
2023-02-03 22:24:30 -0300

    RI-RamonAbreu("Seiviço Pessoas")

--- front/src/app/pessoa/pessoa.module.ts
+++ front/src/app/pessoa/pessoa.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PessoaService } from './services/pessoa.service';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers:[
+    PessoaService,
   ]
 })
 export class PessoaModule { }
