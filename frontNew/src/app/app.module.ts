ab63bc004d62fa345f9b949606484bfc8be6e9d9
ramonabreu-data <cdevabreu@gmail.com>
2023-02-06 17:31:29 -0300

    RI-RamonAbreu("Módulo pessoa")

--- frontNew/src/app/app.module.ts
+++ frontNew/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PessoaModule } from './pessoa/pessoa.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PessoaModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
