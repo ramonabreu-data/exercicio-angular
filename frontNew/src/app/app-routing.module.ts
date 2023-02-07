36e9c5bf32791eb9f35a7a6380b16f241d584dc4
ramonabreu-data <cdevabreu@gmail.com>
2023-02-06 22:45:11 -0300

    RI-RamonAbreu("Inserir Pessoa")

--- frontNew/src/app/app-routing.module.ts
+++ frontNew/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InserirPessoaComponent } from './pessoa/inserir-pessoa/inserir-pessoa.component';
 import { ListarPessoaComponent } from './pessoa/listar-pessoa/listar-pessoa.component';
 
 const routes: Routes = [
@@ -9,7 +10,9 @@ pathMatch: 'full' },
 { path: 'pessoas',
 redirectTo: 'pessoas/listar' },
 { path: 'pessoas/listar',
-component: ListarPessoaComponent }
+component: ListarPessoaComponent },
+{path: 'pessoas/novo',
+  component: InserirPessoaComponent}
 ];
 
 @NgModule({
