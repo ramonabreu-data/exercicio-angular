dce5d73807fdf3e62359612062e90ba1637d2756
ramonabreu-data <cdevabreu@gmail.com>
2023-02-07 22:18:33 -0300

    RI-RamonAbreu("Testes Inserir Pessoa")

--- frontNew/src/app/pessoa/inserir-pessoa/inserir-pessoa.component.spec.ts
+++ frontNew/src/app/pessoa/inserir-pessoa/inserir-pessoa.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { Pessoa } from 'src/app/shared/models/pessoa.model';
+import { PessoaService } from '../services/pessoa.service';
 
 import { InserirPessoaComponent } from './inserir-pessoa.component';
 
@@ -17,6 +20,45 @@ describe('InserirPessoaComponent', () => {
     fixture.detectChanges();
   });
 
+  it('Deve inserir uma pessoa se o formulário for válido', () => {
+    const pessoaService = TestBed.get(PessoaService);
+    spyOn(pessoaService, 'inserir');
+    const component = TestBed.createComponent(InserirPessoaComponent).componentInstance;
+    component.formPessoa.form.setValue({ nome: 'TRamon', cargo: 'TAdvogado', Setor:'TVara'});
+    component.inserir();
+    expect(pessoaService.inserir).toHaveBeenCalledWith(new Pessoa());
+  });
+  
+  it('Não deve inserir uma pessoa se o formulário não for válido', () => {
+    const pessoaService = TestBed.get(PessoaService);
+    spyOn(pessoaService, 'inserir');
+    const component = TestBed.createComponent(InserirPessoaComponent).componentInstance;
+    component.formPessoa.form.setValue({ nome: '', cargo: '',setor:'' });
+    component.inserir();
+    expect(pessoaService.inserir).not.toHaveBeenCalled();
+  });
+
+  it('Deve navegar para "/pessoas" após a inserção da pessoa', () => {
+    const pessoaService = TestBed.get(PessoaService);
+    spyOn(pessoaService, 'inserir');
+    const router = TestBed.get(Router);
+    spyOn(router, 'navigate');
+    const component = TestBed.createComponent(InserirPessoaComponent).componentInstance;
+    component.formPessoa.form.setValue({nome: '', cargo: '', Setor:'' });
+    component.inserir();
+    expect(router.navigate).toHaveBeenCalledWith(['/pessoas']);
+  });
+
+  it('Deve injetar o serviço PessoaService corretamente', () => {
+    const pessoaService = TestBed.get(PessoaService);
+    expect(pessoaService).toBeTruthy();
+  });
+
+  it('Deve injetar o serviço Router corretamente', () => {
+    const router = TestBed.get(Router);
+    expect(router).toBeTruthy();
+  });
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
